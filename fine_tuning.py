3bb6faef9b77e0181c58028cddb42b9f133a7c98
Param Pal Singh <singhparampal97@gmail.com>
2025-07-27 22:40:28 +0200

    add initial peft setup

--- fine_tuning.py
+++ fine_tuning.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from peft-model!")
+from peft import LoraConfig, TaskType, get_peft_model
+from transformers import AutoModelForSeq2SeqLM
+
+peft_config = LoraConfig(
+    task_type=TaskType.SEQ_2_SEQ_LM,
+    inference_mode=False,
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.1,
+)
 
 
-if __name__ == "__main__":
-    main()
+model = AutoModelForSeq2SeqLM.from_pretrained("bigscience/mt0-large")
+
+model = get_peft_model(model, peft_config)
+model.print_trainable_parameters()
